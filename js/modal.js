cc1e2bbae5e3eea51a314de16206c8984e28e210
okeeffeme <okeeffeme@gmail.com>
2017-09-26 15:48:54 +0200

    Fixing example modal

--- js/modal.js
+++ js/modal.js
@@ -4,7 +4,26 @@ var modal = document.getElementById('myModal');
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
+// Get confirmation
+var confirm = document.getElementById('confirmSubmit');
+
+// Enable or Disable button
+function acceptTerms() {
+  if (!confirm.checked) {
+    btn.disabled = true;
+  } else {
+    btn.disabled = false;
+  }
+};
+
 // When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
+
+// For display purposes only, remove for production
+document.querySelector("#myBtn").addEventListener("click", function(event){
+    event.preventDefault();
+}, false);
+
+window.onload = acceptTerms();
